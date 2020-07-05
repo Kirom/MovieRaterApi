392e1a14950642d1fdfba80cf14adf7ca5d29c64
Nizhdanchik <Nizhdanchik@gmail.com>
2020-07-05 21:43:36 +0300

    Added Token Auth

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.shortcuts import render
 from rest_framework import viewsets, status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -12,6 +13,7 @@ from api.serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
 
     # authentication_classes =
     @action(methods=['POST'], detail=True)
@@ -19,22 +21,25 @@ class MovieViewSet(viewsets.ModelViewSet):
         if 'stars' in request.data:
             movie = Movie.objects.get(pk=pk)
             stars = request.data['stars']
-            user = User.objects.get(id=1)
-        try:
-            rating = Rating.objects.create(movie=movie, user=user, stars=stars)
-            serializer = RatingSerializer(rating, many=False)
-            response = {'message': 'Rating created!', 'result': serializer.data}
-            return Response(response)
-        except IntegrityError as exc:
-            if 'UNIQUE' in str(exc):
-                rating = Rating.objects.get(movie=movie, user=user)
-                rating.stars = stars
-                rating.save()
+            # user = User.objects.get(id=1)
+            user = request.user
+            try:
+                rating = Rating.objects.create(movie=movie, user=user, stars=stars)
                 serializer = RatingSerializer(rating, many=False)
-                response = {'message': 'Rating updated!', 'result': serializer.data}
+                response = {'message': 'Rating created!', 'result': serializer.data}
                 return Response(response)
+            except IntegrityError as exc:
+                if 'UNIQUE' in str(exc):
+                    rating = Rating.objects.get(movie=movie, user=user)
+                    rating.stars = stars
+                    rating.save()
+                    serializer = RatingSerializer(rating, many=False)
+                    response = {'message': 'Rating updated!', 'result': serializer.data}
+                    return Response(response)
+        return Response({'message': 'You need to provide stars'})
 
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
